941bcb486eabc8f8fd6e9fe149f4668296a2f578
Ash Wilson <smashwilson@gmail.com>
2015-02-26 20:38:36 -0600

    Requirements

--- setup.py
+++ setup.py
@@ -14,9 +14,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
-requirements = [
-    # TODO: put package requirements here
-]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().split("\n")
 
 test_requirements = [
     # TODO: put package test requirements here
