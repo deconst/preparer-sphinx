0d70cf8b358dc983ad3e81366b162b27154954bf
Ash Wilson <smashwilson@gmail.com>
2015-04-03 15:47:12 -0400

    Hook post_process_images to upload them.

--- deconstrst/builder.py
+++ deconstrst/builder.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
+from os import path
+
+import requests
+from docutils import nodes
 from sphinx.builders.html import JSONHTMLBuilder
 from sphinx.util import jsonimpl
+from deconstrst.config import Configuration
 
 
 class DeconstJSONImpl:
@@ -42,3 +48,36 @@ class DeconstJSONBuilder(JSONHTMLBuilder):
 
     def init(self):
         JSONHTMLBuilder.init(self)
+
+        self.deconst_config = Configuration(os.environ)
+        self.should_submit = not self.deconst_config.skip_submit_reasons()
+
+    def finish(self):
+        """
+        We need to write images and static assets *first*.
+
+        Also, the search indices and so on aren't necessary.
+        """
+
+    def post_process_images(self, doctree):
+        """
+        Publish images to the content store. Modify the image reference with
+        the
+        """
+
+        JSONHTMLBuilder.post_process_images(self, doctree)
+
+        if self.should_submit:
+            for node in doctree.traverse(nodes.image):
+                node['uri'] = self._publish_entry(node['uri'])
+
+    def _publish_entry(self, srcfile):
+        # TODO guess the content-type
+
+        url = self.deconst_config.content_store_url + "assets"
+        basename = path.basename(srcfile)
+        files = {basename: open(srcfile, 'rb')}
+
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+        return response.json()[basename]
