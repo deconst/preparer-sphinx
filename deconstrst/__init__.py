d4b6f8b6acddc399c063eb05dd785bc44ddac666
Ash Wilson <smashwilson@gmail.com>
2016-05-26 16:08:14 -0400

    Install dependencies from a requirements.txt file.

--- deconstrst/__init__.py
+++ deconstrst/__init__.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+from pip import pip
 from deconstrst.deconstrst import build, get_conf_builder
 from deconstrst.config import Configuration
 
@@ -31,6 +32,9 @@ def main(directory=False):
     os.makedirs(config.envelope_dir, exist_ok=True)
     os.makedirs(config.asset_dir, exist_ok=True)
 
+    # Install pip requirements when possible.
+    install_requirements()
+
     # Lock source and destination to the same paths as the Makefile.
     srcdir = '.'
     destdir = os.path.join('_build', get_conf_builder(srcdir))
@@ -48,6 +52,30 @@ def main(directory=False):
         print(file=sys.stderr)
         sys.exit(1)
 
+def install_requirements():
+    """
+    Install non-colliding dependencies from a "requirements.txt" file found at
+    the content root.
+    """
+
+    if not os.path.exists('requirements.txt'):
+        return
+
+    dependencies = []
+
+    with open('requirements.txt', 'r', encoding='utf-8') as rf:
+        for line in rf:
+            if line.startswith('#'):
+                continue
+
+            stripped = line.strip()
+            if not stripped:
+                continue
+
+            dependencies.append(stripped)
+
+    print("Installing dependencies: {}.".format(', '.join(dependencies)))
+    pip.main(['install', '--quiet'] + dependencies)
 
 if __name__ == '__main__':
     main()
